c72a5d679357b9dd84eebce5f7104bded1b41c72
Mateus Alves <98139059+Redyf@users.noreply.github.com>
2023-05-03 17:12:17 -0300

    updated dotfiles
    New plugins added, new kitty colorscheme (dark-decay) and changed blur for hyprland compositor

--- lvim/config.lua
+++ lvim/config.lua
@@ -10,9 +10,9 @@ vim.g.neovide_cursor_vfx_particle_lifetime = 5.0
 vim.g.neovide_cursor_vfx_particle_density = 14.0
 vim.g.neovide_cursor_vfx_particle_speed = 12.0
 -- vim.o.guifont = "CaskaydiaCove Nerd Font:h13:b"
--- vim.o.guifont = "JetBrainsMono Nerd Font:h12:b"
+vim.o.guifont = "JetBrainsMono Nerd Font:h12:b"
 -- vim.o.guifont = "FiraCode Nerd Font:h15:b"
-vim.o.guifont = "SF Mono:h16:b"
+-- vim.o.guifont = "SF Mono:h16:b"
 -- vim.o.guifont = "BlexMono Nerd Font Mono:h16:b"
 
 -- vim options
@@ -54,7 +54,7 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 -- lvim.builtin.which_key.mappings["W"] = { "<cmd>noautocmd w<cr>", "Save without formatting" }
 -- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
 
-lvim.colorscheme = "dark-decay" -- 'oh-lucy' and 'oh-lucy-evening'
+lvim.colorscheme = "dark-decay"
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.terminal.active = true
@@ -65,7 +65,6 @@ lvim.builtin.terminal.open_mapping = "<C-t>"
 
 -- Automatically install missing parsers when entering buffer
 lvim.builtin.treesitter.auto_install = true
-
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 
 -- lvim.lsp.installer.setup.automatic_installation = false
@@ -113,45 +112,6 @@ lvim.lsp.automatic_configuration.skipped_servers = {
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.offsetEncoding = { "utf-8" }
 
-local formatters = require("lvim.lsp.null-ls.formatters")
-formatters.setup({
-	{ command = "zprint", filetypes = { "clojure" } },
-	{ command = "stylua", filetypes = { "lua" } },
-	{ command = "black", filetypes = { "python" } },
-	{
-		command = "prettier",
-		extra_args = { "--print-width", "100" },
-		filetypes = { "typescript", "typescriptreact" },
-	},
-	{ command = "alejandra", filetypes = { "nix" } },
-	-- { command = "astyle", filetypes = { "c", "cpp", "objc", "objcpp" }, offsetEncoding = "utf-8" },
-})
-local linters = require("lvim.lsp.null-ls.linters")
-linters.setup({
-	{
-		command = "flake8",
-		filetypes = { "python" },
-		extra_args = { "--ignore", "E501, E275, F841" },
-	},
-	{ command = "shellcheck", args = { "--severity", "warning" } }, -- {
-	--   command = "codespell",
-	--   filetypes = { "javascript", "python" },
-	-- },
-	-- {
-	--     command = "luacheck",
-	--     filetypes = {"lua"},
-	--     extra_args = {"--ignore", "112, 631"}
-	-- },
-	{
-		command = "clj_kondo",
-		filetypes = { "clojure" },
-		-- extra_args = {"--ignore", "112, 631"}
-	},
-	{ command = "selene", filetypes = { "lua" } },
-	{ command = "statix", filetypes = { "nix" } },
-	-- { command = "cpplint", filetypes = { "c", "cpp", "objc", "objcpp" }, offsetEncoding = "utf-8" },
-})
-
 -- Enable LSP
 require("lspconfig").clojure_lsp.setup({
 	command = "/home/redyf/.nix-profile/bin/clojure-lsp",
@@ -194,6 +154,45 @@ require("lspconfig").dartls.setup({})
 -- 	filetypes = { "c", "cpp", "objc", "objcpp" },
 -- })
 
+-- Linters
+local linters = require("lvim.lsp.null-ls.linters")
+linters.setup({
+	{
+		command = "flake8",
+		filetypes = { "python" },
+		extra_args = { "--ignore", "E501, E275, F841" },
+	},
+	{ command = "shellcheck", args = { "--severity", "warning" } }, -- {
+	--   command = "codespell",
+	--   filetypes = { "javascript", "python" },
+	-- },
+	{
+		command = "clj_kondo",
+		filetypes = { "clojure" },
+		-- extra_args = {"--ignore", "112, 631"}
+	},
+	{ command = "selene", filetypes = { "lua" } },
+	{ command = "statix", filetypes = { "nix" } },
+	-- { command = "cpplint", filetypes = { "c", "cpp", "objc", "objcpp" }, offsetEncoding = "utf-8" },
+})
+
+-- Formatters
+local formatters = require("lvim.lsp.null-ls.formatters")
+formatters.setup({
+	{ command = "zprint", filetypes = { "clojure" } },
+	{ command = "stylua", filetypes = { "lua" } },
+	{ command = "black", filetypes = { "python" } },
+	{
+		command = "prettier",
+		extra_args = { "--print-width", "100" },
+		filetypes = { "typescript", "typescriptreact" },
+	},
+	{ command = "alejandra", filetypes = { "nix" } },
+	-- { command = "astyle", filetypes = { "c", "cpp", "objc", "objcpp" }, offsetEncoding = "utf-8" },
+	{ command = "dart_format", filetypes = { "dart" } },
+})
+
+-- Debuggers
 local dap = require("dap")
 dap.configurations.elixir = {
 	{
@@ -234,14 +233,6 @@ dap.configurations.dart = {
 	},
 }
 
--- Original, teste para ver se o erro loop.lua continua (null-ls)
--- local dap = require('dap')
---   dap.adapters.python = {
---     type = 'executable';
---     command = os.getenv('HOME') .. '/.virtualenvs/debugpy/bin/python';
---     args = { '-m', 'debugpy.adapter' };
---   }
-
 local dap = require("dap")
 dap.configurations.python = {
 	{
@@ -279,8 +270,9 @@ lvim.plugins = {
 	{ "catppuccin/nvim", name = "catppuccin" },
 	{ "decaycs/decay.nvim", name = "decay" },
 	{ "tiagovla/tokyodark.nvim" },
-	{ "Yazeed1s/oh-lucy.nvim" }, -- Dap
-	{ "mfussenegger/nvim-dap-python" }, -- Other tools
+	-- Dap
+	{ "mfussenegger/nvim-dap-python" },
+	-- Other tools
 	{ "nvim-neorg/neorg" },
 	{ "norcalli/nvim-colorizer.lua" },
 	{ "andweeb/presence.nvim" },
@@ -289,8 +281,7 @@ lvim.plugins = {
 	{ "Olical/aniseed" },
 	{ "tpope/vim-dispatch" },
 	{ "clojure-vim/vim-jack-in" },
-	{ "radenling/vim-dispatch-neovim" }, -- Fun
-	{ "ThePrimeagen/vim-be-good" },
+	{ "radenling/vim-dispatch-neovim" },
 	-- {
 	-- 	"akinsho/flutter-tools.nvim",
 	-- 	lazy = false,
@@ -299,8 +290,12 @@ lvim.plugins = {
 	-- 		"stevearc/dressing.nvim", -- optional for vim.ui.select
 	-- 	},
 	-- },
+	--
+	-- Fun
+	{ "ThePrimeagen/vim-be-good" },
 }
 
+-- Neorg plugin for notes
 require("neorg").setup({
 	load = {
 		["core.defaults"] = {},
@@ -312,6 +307,7 @@ require("neorg").setup({
 	},
 })
 
+-- Discord Presence plugin
 require("presence"):setup({
 	file_assets = {
 		c = { "C ", "https://raw.githubusercontent.com/leonardssh/vscord/main/assets/icons/c.png" },
@@ -433,7 +429,7 @@ require("presence"):setup({
 	client_id = "793271441293967371",
 	show_time = true,
 	workspace_text = function()
-		return "I see C you :skull:"
+		return "I C you :skull:"
 	end,
 })
 
@@ -448,251 +444,3 @@ require("colorizer").setup()
 --     require("nvim-treesitter.highlight").attach(0, "bash")
 --   end,
 -- })
-
-require("catppuccin").setup({
-	flavour = "mocha", -- Can be one of: latte, frappe, macchiato, mocha
-	background = { light = "latte", dark = "mocha" },
-	dim_inactive = {
-		enabled = false,
-		-- Dim inactive splits/windows/buffers.
-		-- NOT recommended if you use old palette (a.k.a., mocha).
-		shade = "dark",
-		percentage = 0.15,
-	},
-	transparent_background = transparent_background,
-	show_end_of_buffer = false, -- show the '~' characters after the end of buffers
-	term_colors = true,
-	compile_path = vim.fn.stdpath("cache") .. "/catppuccin",
-	styles = {
-		comments = { "italic" },
-		properties = { "italic" },
-		functions = { "italic", "bold" },
-		keywords = { "italic" },
-		operators = { "bold" },
-		conditionals = { "bold" },
-		loops = { "bold" },
-		booleans = { "bold", "italic" },
-		numbers = {},
-		types = {},
-		strings = {},
-		variables = {},
-	},
-	integrations = {
-		treesitter = true,
-		native_lsp = {
-			enabled = true,
-			virtual_text = {
-				errors = { "italic" },
-				hints = { "italic" },
-				warnings = { "italic" },
-				information = { "italic" },
-			},
-			underlines = {
-				errors = { "underline" },
-				hints = { "underline" },
-				warnings = { "underline" },
-				information = { "underline" },
-			},
-		},
-		aerial = false,
-		barbar = false,
-		beacon = false,
-		cmp = true,
-		coc_nvim = false,
-		dap = { enabled = true, enable_ui = true },
-		dashboard = false,
-		fern = false,
-		fidget = true,
-		gitgutter = false,
-		gitsigns = true,
-		harpoon = false,
-		hop = true,
-		illuminate = true,
-		indent_blankline = { enabled = true, colored_indent_levels = false },
-		leap = false,
-		lightspeed = false,
-		lsp_saga = true,
-		lsp_trouble = true,
-		markdown = true,
-		mason = true,
-		mini = false,
-		navic = { enabled = false },
-		neogit = false,
-		neotest = false,
-		neotree = { enabled = false, show_root = true, transparent_panel = false },
-		noice = false,
-		notify = true,
-		nvimtree = true,
-		overseer = false,
-		pounce = false,
-		semantic_tokens = false,
-		symbols_outline = false,
-		telekasten = false,
-		telescope = true,
-		treesitter_context = false,
-		ts_rainbow = true,
-		vim_sneak = false,
-		vimwiki = false,
-		which_key = true,
-	},
-	color_overrides = {
-		mocha = {
-			rosewater = "#F5E0DC",
-			flamingo = "#F2CDCD",
-			mauve = "#DDB6F2",
-			pink = "#F5C2E7",
-			red = "#F28FAD",
-			maroon = "#E8A2AF",
-			peach = "#F8BD96",
-			yellow = "#FAE3B0",
-			green = "#ABE9B3",
-			blue = "#96CDFB",
-			sky = "#89DCEB",
-			teal = "#B5E8E0",
-			lavender = "#C9CBFF",
-
-			text = "#D9E0EE",
-			subtext1 = "#BAC2DE",
-			subtext0 = "#A6ADC8",
-			overlay2 = "#C3BAC6",
-			overlay1 = "#988BA2",
-			overlay0 = "#6E6C7E",
-			surface2 = "#6E6C7E",
-			surface1 = "#575268",
-			surface0 = "#302D41",
-
-			base = "#1E1E2E",
-			mantle = "#1A1826",
-			crust = "#161320",
-		},
-	},
-	highlight_overrides = {
-		mocha = function(cp)
-			return {
-				-- For base configs.
-				NormalFloat = {
-					fg = cp.text,
-					bg = transparent_background and cp.none or cp.base,
-				},
-				CursorLineNr = { fg = cp.green },
-				Search = { bg = cp.surface1, fg = cp.pink, style = { "bold" } },
-				IncSearch = { bg = cp.pink, fg = cp.surface1 },
-				Keyword = { fg = cp.pink },
-				Type = { fg = cp.blue },
-				Typedef = { fg = cp.yellow },
-				StorageClass = { fg = cp.red, style = { "italic" } },
-
-				-- For native lsp configs.
-				DiagnosticVirtualTextError = { bg = cp.none },
-				DiagnosticVirtualTextWarn = { bg = cp.none },
-				DiagnosticVirtualTextInfo = { bg = cp.none },
-				DiagnosticVirtualTextHint = { fg = cp.rosewater, bg = cp.none },
-
-				DiagnosticHint = { fg = cp.rosewater },
-				LspDiagnosticsDefaultHint = { fg = cp.rosewater },
-				LspDiagnosticsHint = { fg = cp.rosewater },
-				LspDiagnosticsVirtualTextHint = { fg = cp.rosewater },
-				LspDiagnosticsUnderlineHint = { sp = cp.rosewater },
-
-				-- For fidget.
-				FidgetTask = { bg = cp.none, fg = cp.surface2 },
-				FidgetTitle = { fg = cp.blue, style = { "bold" } },
-
-				-- For trouble.nvim
-				TroubleNormal = { bg = cp.base },
-
-				-- For treesitter.
-				["@field"] = { fg = cp.rosewater },
-				["@property"] = { fg = cp.yellow },
-
-				["@include"] = { fg = cp.teal },
-				-- ["@operator"] = { fg = cp.sky },
-				["@keyword.operator"] = { fg = cp.sky },
-				["@punctuation.special"] = { fg = cp.maroon },
-
-				-- ["@float"] = { fg = cp.peach },
-				-- ["@number"] = { fg = cp.peach },
-				-- ["@boolean"] = { fg = cp.peach },
-
-				["@constructor"] = { fg = cp.lavender },
-				-- ["@constant"] = { fg = cp.peach },
-				-- ["@conditional"] = { fg = cp.mauve },
-				-- ["@repeat"] = { fg = cp.mauve },
-				["@exception"] = { fg = cp.peach },
-
-				["@constant.builtin"] = { fg = cp.lavender },
-				-- ["@function.builtin"] = { fg = cp.peach, style = { "italic" } },
-				-- ["@type.builtin"] = { fg = cp.yellow, style = { "italic" } },
-				["@type.qualifier"] = { link = "@keyword" },
-				["@variable.builtin"] = { fg = cp.red, style = { "italic" } },
-
-				-- ["@function"] = { fg = cp.blue },
-				["@function.macro"] = { fg = cp.red, style = {} },
-				["@parameter"] = { fg = cp.rosewater },
-				["@keyword"] = { fg = cp.red, style = { "italic" } },
-				["@keyword.function"] = { fg = cp.maroon },
-				["@keyword.return"] = { fg = cp.pink, style = {} },
-
-				-- ["@text.note"] = { fg = cp.base, bg = cp.blue },
-				-- ["@text.warning"] = { fg = cp.base, bg = cp.yellow },
-				-- ["@text.danger"] = { fg = cp.base, bg = cp.red },
-				-- ["@constant.macro"] = { fg = cp.mauve },
-
-				-- ["@label"] = { fg = cp.blue },
-				["@method"] = { fg = cp.blue, style = { "italic" } },
-				["@namespace"] = { fg = cp.rosewater, style = {} },
-
-				["@punctuation.delimiter"] = { fg = cp.teal },
-				["@punctuation.bracket"] = { fg = cp.overlay2 },
-				-- ["@string"] = { fg = cp.green },
-				-- ["@string.regex"] = { fg = cp.peach },
-				["@type"] = { fg = cp.yellow },
-				["@variable"] = { fg = cp.text },
-				["@tag.attribute"] = { fg = cp.mauve, style = { "italic" } },
-				["@tag"] = { fg = cp.peach },
-				["@tag.delimiter"] = { fg = cp.maroon },
-				["@text"] = { fg = cp.text },
-
-				-- ["@text.uri"] = { fg = cp.rosewater, style = { "italic", "underline" } },
-				-- ["@text.literal"] = { fg = cp.teal, style = { "italic" } },
-				-- ["@text.reference"] = { fg = cp.lavender, style = { "bold" } },
-				-- ["@text.title"] = { fg = cp.blue, style = { "bold" } },
-				-- ["@text.emphasis"] = { fg = cp.maroon, style = { "italic" } },
-				-- ["@text.strong"] = { fg = cp.maroon, style = { "bold" } },
-				-- ["@string.escape"] = { fg = cp.pink },
-
-				-- ["@property.toml"] = { fg = cp.blue },
-				-- ["@field.yaml"] = { fg = cp.blue },
-
-				-- ["@label.json"] = { fg = cp.blue },
-
-				["@function.builtin.bash"] = { fg = cp.red, style = { "italic" } },
-				["@parameter.bash"] = { fg = cp.yellow, style = { "italic" } },
-
-				["@field.lua"] = { fg = cp.lavender },
-				["@constructor.lua"] = { fg = cp.flamingo },
-
-				["@constant.java"] = { fg = cp.teal },
-
-				["@property.typescript"] = {
-					fg = cp.lavender,
-					style = { "italic" },
-				},
-				-- ["@constructor.typescript"] = { fg = cp.lavender },
-
-				-- ["@constructor.tsx"] = { fg = cp.lavender },
-				-- ["@tag.attribute.tsx"] = { fg = cp.mauve },
-
-				["@type.css"] = { fg = cp.lavender },
-				["@property.css"] = { fg = cp.yellow, style = { "italic" } },
-
-				["@type.builtin.c"] = { fg = cp.yellow, style = {} },
-
-				["@property.cpp"] = { fg = cp.text },
-				["@type.builtin.cpp"] = { fg = cp.yellow, style = {} },
-
-				-- ["@symbol"] = { fg = cp.flamingo },
-			}
-		end,
-	},
-})
